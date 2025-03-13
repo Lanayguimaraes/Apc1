2c36d968f9f304fdc6ea38c8a5e7bf6e248c8fa1
Lanay <laanyguimaraes@gmail.com>
2025-03-12 21:49:42 -0300

    codigos da aula 02

---
+++ aulas/aula02/imprimir.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    printf("Boa Noite\n\n"); // (\n sempre antes de acabar as aspas ( tudo que for imprimir dentro das aspas)  )
+    printf("\tEstamos Aprendendo C!");
+    return 0;
+
+}
