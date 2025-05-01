22b78a5f0669756bc4513d0e9a503100817ef005
Lanay <laanyguimaraes@gmail.com>
2025-04-30 21:55:17 -0300

    aulas

--- praticas/pratica06/estrutura_if.c
+++ praticas/pratica06/estrutura_if.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int numero = 0; 
+    int numero = 0 ; 
 
 printf("Digite um numero inteiro: ") ;
 scanf("%i", &numero) ;
@@ -10,10 +10,44 @@ int numero_eh_divisivel_por_2 = numero % 2 == 0;
 
 if (numero_eh_divisivel_por_2){
     printf("O numero %i eh par!\n", numero) ;
+    
+} else { 
+    printf("O numero %i eh impar!\n", numero);
+}
+   
+   
+   int idade ;
+    
+    printf("Digite sua idade: ");
+    scanf("%i", &idade); 
 
-
+if ( idade < 16) {
+    printf("Voce naum pode votar!\n");
+} else if ( idade < 18 || idade > 70) {
+        printf("Voce pode votar!\n");
+} else {
+    printf("Voce eh obrigado a votar!\n");
 }
 
+    float media ;
+    printf("Digite sua media final:");
+    scanf("%f", &media);
+
+    if (media == 0.0f) {
+        printf("A mencao eh 'SR'.\n");
+    }   else if (media < 3.0f) {
+            printf("A mencao eh II. \n");
+    }   else if (media < 5.0f) {
+        printf("A mencao eh 'MI'.\n");   
+    }   else if (media < 7.0f) { 
+        printf("A mencao eh 'MS'. \n");
+    }   else if (media < 9.0f) {
+        printf("A mencao eh 'MS'. \n");
+    }   else {
+        printf("A mencao eh 'SS'. \n");
+    }
+
+    
 
 
 
